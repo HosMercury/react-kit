fd98ff5e5185ce7cf6ada92404324c925e89fc76
HosMercury <NaJo@Hos81g>
2025-03-31 02:22:26 +0200

    adding menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,16 @@ import { ThemeToggle } from "./ThemeToggle";
 const Header = () => {
   return (
     <div className="w-full h-13 p-2 bg-violet-900 text-white px-12">
-      <div className="flex justify-between">
-        <div>Logo</div>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-6 items-center">
+          <div>Logo</div>
+          {/* menu */}
+
+          <div className="flex gap-4">
+            <div className="cursor-pointer">Home</div>
+            <div className="cursor-pointer">About</div>
+          </div>
+        </div>
         <div className="flex gap-5">
           <ThemeToggle />
           <UserMenu />
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import Header from "../header/Header";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div className="">
       <Header />
       <div className="px-12 p-2">{children}</div>
     </div>
